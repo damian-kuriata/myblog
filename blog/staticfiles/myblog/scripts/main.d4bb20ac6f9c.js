cbe93e23042d859e06c81c3699cb6fa12a308f9f
damian-kuriata <damian.kuriata2001@gmail.com>
2021-01-22 02:05:08 +0100

    Remove staticfiles

--- blog/staticfiles/myblog/scripts/main.d4bb20ac6f9c.js
+++
@@ -1,9 +0,0 @@
-import {handleMobileNavigation} from "./modules/mobile.js";
-import {handleComments} from "./modules/comments.js";
-
-$(document).ready(() => {
-    //const isMobile =  /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-   // console.log(isMobile);
-    handleMobileNavigation();
-    handleComments();
-})
