fed1ab2aee9bc2b90baf528a76cb5b2ae0c926f0
damian-kuriata <damian.kuriata2001@gmail.com>
2021-02-11 23:17:33 +0100

    Add search icon

--- myblog/static/myblog/scripts/main.js
+++ myblog/static/myblog/scripts/main.js
@@ -1,6 +1,6 @@
 import {handleMobileNavigation} from "./modules/mobile.js";
 import {handleCommentsHideShow} from "./modules/comments.js";
-import {handleDesktopNavigation} from "./modules/desktop.js";
+import { handleDesktopNavigation, handleLeftNavigationExpand } from "./modules/desktop.js";
 import { animateLogo } from "./modules/logo.js";
 
 $(document).ready(function() {
@@ -15,6 +15,9 @@ $(document).ready(function() {
     let window_ = $(window);
     let categoriesDropdown = $(".categories-dropdown");
     handleDesktopNavigation(desktopItems, window_.width(), categoriesDropdown);
+    let categoriesExpand = $("aside > .categories-expand");
+    let leftCategoriesPanel = $("aside > .left-categories-panel");
+    handleLeftNavigationExpand(categoriesExpand, leftCategoriesPanel);
     window_.resize(function() {
         handleDesktopNavigation(desktopItems, window_.width(),
             categoriesDropdown);
--- myblog/static/myblog/scripts/modules/desktop.js
+++ myblog/static/myblog/scripts/modules/desktop.js
@@ -10,10 +10,10 @@ function handleDesktopNavigation(desktopItems,
     if (handleDesktopNavigation.previousWindowWidth === undefined) {
         handleDesktopNavigation.previousWindowWidth = -1;
     }
+    let desktopItemsWidth = desktopItems.width();
     if (handleDesktopNavigation.previousWindowWidth === -1 ||
-        handleDesktopNavigation.previousWindowWidth > windowWidth)  {
+        handleDesktopNavigation.previousWindowWidth > windowWidth) {
         handleDesktopNavigation.previousWindowWidth = windowWidth;
-        let desktopItemsWidth = desktopItems.width();
         while (desktopItemsWidth + 0.05 * windowWidth >= 0.8 * windowWidth) {
             let desktopItemsChildren = desktopItems.children();
             desktopItemsChildren.last().addClass("display-block");
@@ -21,6 +21,41 @@ function handleDesktopNavigation(desktopItems,
             desktopItemsWidth = desktopItems.width();
         }
     }
+    /*
+    else if (handleDesktopNavigation.previousWindowWidth < windowWidth) {
+        handleDesktopNavigation.previousWindowWidth = windowWidth;
+        console.log("Smaller");
+        let desktopItemsWidth = desktopItems.width();
+        while (desktopItemsWidth + 0.05 * windowWidth < 0.8 * windowWidth) {
+            let desktopItemsChildren = desktopItems.children();
+            categoriesDropdown.eq(-1).detach().appendTo(desktopItemsChildren);
+            desktopItemsWidth = desktopItems.width();
+        }
+    }*/
 }
 
-export {handleDesktopNavigation};
+function handleLeftNavigationExpand(categoriesExpand, leftCategoriesPanel) {
+    /*
+     Controls whether expand button is visible or not
+     */
+    const animationDuration = 400;
+    leftCategoriesPanel.find(".categories-shrink").click(() => {
+        leftCategoriesPanel.toggleClass("visibility-hidden");
+        categoriesExpand.toggleClass("visibility-hidden");
+    });
+    categoriesExpand.click(() => {
+        leftCategoriesPanel.toggleClass("visibility-hidden");
+        //categoriesExpand.addClass("visibility-hidden");
+        //categoriesExpand.hasClass("visibility-hidden")?categoriesExpand.removeClass("visibility-hidden"):categoriesExpand.addClass("visibility-hidden");
+        /* TODO: Write an animation */
+        /*console.log(leftCategoriesPanel.width());
+        leftCategoriesPanel.animate({
+            width: "toggle"
+        }, animationDuration, () => {
+            categoriesExpand.toggleClass("visibility-hidden");
+            leftCategoriesPanel.toggleClass("visibility-hidden");
+        });*/
+    });
+}
+
+export { handleDesktopNavigation, handleLeftNavigationExpand };
